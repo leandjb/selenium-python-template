9e2bd9d904ab5d3fd25ed162ea2e3b682062ad74
leandjb <leandjb@gmail.com>
2024-04-28 23:23:05 -0500

    added base_page common interaction methods (type text, click on element, select option, etc). LB

--- src/pages/base_page.py
+++ src/pages/base_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 
 
@@ -16,4 +17,26 @@ class BasePage:
     def navigate_to_url(self, url):
         self.driver.get(url)
 
+    def type_text(self, locator, text):
+        self.wait_for_element(locator).clear()
+        self.wait_for_element(locator).send_keys(text)
 
+    def select_text_from_dropdown(self, locator, text):
+        select = Select(self.wait_for_element(locator))
+        select.select_by_visible_text(text)
+
+    def select_index_from_dropdown(self, locator, index):
+        select = Select(self.wait_for_element(locator))
+        select.select_by_index(index)
+
+    def select_element_btn(self, locator):
+        """Select checkbox or radiobutton if it is not selected"""
+        checkbox = self.wait_for_element(locator)
+        if not checkbox.is_selected():
+            checkbox.click()
+
+    def unselect_checkbox(self, locator):
+        """Unselect the checkbox or radiobutton if it is selected"""
+        checkbox = self.wait_for_element(locator)
+        if checkbox.is_selected():
+            checkbox.click()
